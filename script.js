59e9c6898189c9a85098704bf0333d1a29e2d37d
satyam <2019335@iiitdmj.ac.in>
2021-05-31 12:27:45 +0530

    Smooth Scrolls Added

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 'use strict';
-
 ///////////////////////////////////////
 // Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +17,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(button => button.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +29,53 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+// /// Selecting Elements
+
+//Creating the Element and Inserting it at the same time
+const header = document.querySelector('.header');
+console.log(header);
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = ` We use it to improve the Functionlity of the page <button class
+="btn btn--close-cookie"> got it!! </button>`;
+header.append(message);
+
+//Deleting
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+//Styling Our Created Elements
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+///Smooth Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  //note: BoundedClientRect is relative with the current visible ViewPort
+  const s1cord = section1.getBoundingClientRect(); //target loc
+  // console.log('current scroll x , y =', window.pageXOffset, window.pageYOffset);
+  //THis will keep trace of how much you have been scrolled
+  // window.scrollTo(
+  //   s1cord.left + window.pageXOffset,
+  //   s1cord.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1cord.left + window.pageXOffset,
+  //   top: s1cord.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
